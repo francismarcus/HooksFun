6102169772bc21779bd46cd5254b3d924d91570c
Marcus Francis <marcusmagnusson@outlook.com>
2019-03-23 08:23:51 +0100

    Moving things around

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import ClickCounter from './clickCounter/ClickCounter'
+
+const App = () => {
+  return (
+    <div>
+      <ClickCounter />
+    </div>
+  )
+}
+
+export default App;
